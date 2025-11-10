e895cb6ab6e31a2c0351b6b7f56e6fd2653aa83b
Destin Mazoma <mokonzidestin@icloud.com>
2025-11-10 13:12:11 +0300

    Fix: correct 7-multi_c.js loop logic

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,9 @@
 const x = parseInt(process.argv[2]);
 
-if (!x || x <= 0) {
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
-} else {
-  for (let i = 0; i < x; i++) console.log("C is fun");
+} else if (x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
